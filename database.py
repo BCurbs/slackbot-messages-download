5bb4452834f73db65782abe4e9fc6f241db8acda
Ben Curby <bencurby80@gmail.com>
2022-08-15 19:22:56 -0400

    Fix database pool.

--- database.py
+++ database.py
@@ -22,7 +22,7 @@ class Database:
             await conn.execute("""CREATE TABLE IF NOT EXISTS messages (
                     ts DOUBLE PRIMARY KEY,
                     id VARCHAR NOT NULL,
-                    user VARCHAR NOT NULL,
+                    user_id VARCHAR NOT NULL,
                     text TEXT NOT NULL
                     )""")
         logger.debug("Created tables")
@@ -30,7 +30,7 @@ class Database:
     async def new_message(self, message: Message):
         async with self.pool.acquire() as conn:
             await conn.execute("""
-            INSERT OR IGNORE INTO messages(ts, id, user, text)
+            INSERT OR IGNORE INTO messages(ts, id, user_id, text)
             VALUES($1, $2, $3, $4);
             """, message.ts, message.id, message.user, message.text)
         logger.success("Added message to database")
@@ -43,7 +43,7 @@ class Database:
             messages_tuple.append((message.ts, message.id, message.user, message.text))
         async with self.pool.acquire() as conn:
             await conn.executemany("""
-            INSERT OR IGNORE INTO messages(ts, id, user, text)
+            INSERT OR IGNORE INTO messages(ts, id, user_id, text)
             VALUES($1, $2, $3, $4);
             """, messages_tuple)
         logger.success(f"Added {len(messages)} to database")
